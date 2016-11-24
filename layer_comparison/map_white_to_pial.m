73703fe64d5f9e6179884704c4778271b325042b
jbonaiuto <jbonaiuto@gmail.com>
2016-11-24 11:13:42 +0000

    do more efficiently

--- layer_comparison/map_white_to_pial.m
+++ layer_comparison/map_white_to_pial.m
@@ -1,12 +1,13 @@
 function white_pial_map=map_white_to_pial(white, pial)
 
-white_pial_map=zeros(size(pial.vertices,1),1);
+%white_pial_map=zeros(size(pial.vertices,1),1);
 % Loop through all pial vertices
-for i=1:size(pial.vertices,1)
+%for i=1:size(pial.vertices,1)
     % For each pial vertex, compute the distance to every other white vertex
-    pial_white_diff=white.vertices-repmat(pial.vertices(i,:),size(white.vertices,1),1);
-    pial_white_dist=sqrt(sum((pial_white_diff.^2)'));
+%    pial_white_diff=white.vertices-repmat(pial.vertices(i,:),size(white.vertices,1),1);
+%    pial_white_dist=sqrt(sum((pial_white_diff.^2)'));
     % find the closest white vertex to this pial vertex
-    min_dist=find(pial_white_dist==min(pial_white_dist));
-    white_pial_map(i)=min_dist(1);
-end
+%    min_dist=find(pial_white_dist==min(pial_white_dist));
+%    white_pial_map(i)=min_dist(1);
+%end
+white_pial_map=dsearchn(white.vertices,pial.vertices);
