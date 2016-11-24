20801533e070f6e2d80fd9d86d5c3df982670a97
jbonaiuto <jbonaiuto@gmail.com>
2016-11-24 11:13:57 +0000

    compute perc diff from baseline

--- layer_comparison/compare_session_layers.m
+++ layer_comparison/compare_session_layers.m
@@ -1,4 +1,4 @@
-function [pial_diff, white_diff]=compare_session_layers(subj_info, session_num, foi, woi1, woi2, comparison_name, varargin)
+function [pial_perc_change, white_perc_change]=compare_session_layers(subj_info, session_num, foi, woi, baseline, comparison_name, varargin)
 
 % Parse inputs
 defaults = struct('data_dir', '/data/pred_coding', 'save_results', true, 'patch_size', 0.4, 'white_pial_map', []);  %define default values
@@ -20,57 +20,61 @@ foi_dir=fullfile(params.data_dir, 'analysis', subj_info.subj_id, num2str(session
 
 spm('defaults', 'EEG');
 
-woi1_dir=fullfile(foi_dir, ['t' num2str(woi1(1)) '_' num2str(woi1(2))]);
-woi2_dir=fullfile(foi_dir, ['t' num2str(woi2(1)) '_' num2str(woi2(2))]);
+woi_dir=fullfile(foi_dir, ['t' num2str(woi(1)) '_' num2str(woi(2))]);
+baseline_dir=fullfile(foi_dir, ['t' num2str(baseline(1)) '_' num2str(baseline(2))]);
 
-% Load all pial data from woi1
-pial_woi1_trials=[];
-[files,dirs] = spm_select('List', woi1_dir, ['^pial_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(woi1(1)) '_' num2str(woi1(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
+% Load all pial data from woi
+pial_woi_trials=[];
+[files,dirs] = spm_select('List', woi_dir, ['^pial_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(woi(1)) '_' num2str(woi(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
 for f=1:size(files,1)
     filename=deblank(files(f,:));
-    trial_mesh=gifti(fullfile(woi1_dir,filename));
-    pial_woi1_trials(:,end+1)=trial_mesh.cdata(:);
+    trial_mesh=gifti(fullfile(woi_dir,filename));
+    pial_woi_trials(:,end+1)=trial_mesh.cdata(:);
 end
 
-% Load all pial data from woi2
-pial_woi2_trials=[];
-[files,dirs] = spm_select('List', woi2_dir, ['^pial_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(woi2(1)) '_' num2str(woi2(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
+% Load all pial data from baseline
+pial_baseline_trials=[];
+[files,dirs] = spm_select('List', baseline_dir, ['^pial_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(baseline(1)) '_' num2str(baseline(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
 for f=1:size(files,1)
     filename=deblank(files(f,:));
-    trial_mesh=gifti(fullfile(woi2_dir,filename));
-    pial_woi2_trials(:,end+1)=trial_mesh.cdata(:);
+    trial_mesh=gifti(fullfile(baseline_dir,filename));
+    pial_baseline_trials(:,end+1)=trial_mesh.cdata(:);
 end
-pial_diff=pial_woi1_trials-pial_woi2_trials;
+% Compute percentage change from baseline in pial surface
+% TODO - this is per trial baseline, what about mean baseline?
+pial_perc_change=(pial_woi_trials-pial_baseline_trials)./pial_baseline_trials;
 
-% Load all white matter data from woi1
-white_woi1_trials=[];
-[files,dirs] = spm_select('List', woi1_dir, ['^white_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(woi1(1)) '_' num2str(woi1(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
+% Load all white matter data from woi
+white_woi_trials=[];
+[files,dirs] = spm_select('List', woi_dir, ['^white_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(woi(1)) '_' num2str(woi(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
 for f=1:size(files,1)
     filename=deblank(files(f,:));
-    trial_mesh=gifti(fullfile(woi1_dir,filename));
-    white_woi1_trials(:,end+1)=trial_mesh.cdata(:);
+    trial_mesh=gifti(fullfile(woi_dir,filename));
+    white_woi_trials(:,end+1)=trial_mesh.cdata(:);
 end
 
-% Load all white matter data from woi2
-white_woi2_trials=[];
-[files,dirs] = spm_select('List', woi2_dir, ['^white_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(woi2(1)) '_' num2str(woi2(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
+% Load all white matter data from baseline
+white_baseline_trials=[];
+[files,dirs] = spm_select('List', baseline_dir, ['^white_r' subj_info.subj_id '_' num2str(session_num) '_1_t' num2str(baseline(1)) '_' num2str(baseline(2)) '_f' num2str(foi(1)) '_' num2str(foi(2)) '.*\.gii']);
 for f=1:size(files,1)
     filename=deblank(files(f,:));
-    trial_mesh=gifti(fullfile(woi2_dir, filename));
-    white_woi2_trials(:,end+1)=trial_mesh.cdata(:);
+    trial_mesh=gifti(fullfile(baseline_dir, filename));
+    white_baseline_trials(:,end+1)=trial_mesh.cdata(:);
 end
-white_diff=white_woi1_trials-white_woi2_trials;
+% Compute percent change from baseline in white matter surface
+% TODO - this is per trial baseline, what about mean baseline?
+white_perc_change=(white_woi_trials-white_baseline_trials)./white_baseline_trials;
 
 if params.save_results
-    % Save pial diff
-    c=file_array(fullfile(foi_dir, ['pial.' comparison_name '.diff.shape.dat']),size(pial_diff,1),'FLOAT32-LE',0,1,0);
-    c(:)=mean(pial_diff,2);
-    pial_diff_surf = gifti;
-    pial_diff_surf.cdata = c;
-    save(pial_diff_surf, fullfile(foi_dir, ['pial.' comparison_name '.diff.shape.gii']), 'ExternalFileBinary');
+    % Save pial percent change
+    c=file_array(fullfile(foi_dir, ['pial.' comparison_name '.perc_change.shape.dat']),size(pial_perc_change,1),'FLOAT32-LE',0,1,0);
+    c(:)=mean(pial_perc_change,2);
+    pial_perc_change_surf = gifti;
+    pial_perc_change_surf.cdata = c;
+    save(pial_perc_change_surf, fullfile(foi_dir, ['pial.' comparison_name '.perc_change.shape.gii']), 'ExternalFileBinary');
 
-    % Compare pial values at two wois
-    [H,pvals,ci,STATS]=ttest(pial_diff');
+    % Compute significance of change from baseline in pial
+    [H,pvals,ci,STATS]=ttest(pial_perc_change');
     pial_tvals=STATS.tstat';
 
     % Save pial comparison
@@ -80,15 +84,15 @@ if params.save_results
     pial_t_surf.cdata = c;
     save(pial_t_surf, fullfile(foi_dir, ['pial.' comparison_name '.t.shape.gii']), 'ExternalFileBinary');
 
-    % Save white matter diff
-    c=file_array(fullfile(foi_dir, ['white.' comparison_name '.diff.shape.dat']),size(white_diff,1),'FLOAT32-LE',0,1,0);
-    c(:)=mean(white_diff,2);
-    white_diff_surf = gifti;
-    white_diff_surf.cdata = c;
-    save(white_diff_surf, fullfile(foi_dir, ['white.' comparison_name '.diff.shape.gii']), 'ExternalFileBinary');
+    % Save white matter percent change
+    c=file_array(fullfile(foi_dir, ['white.' comparison_name '.perc_change.shape.dat']),size(white_perc_change,1),'FLOAT32-LE',0,1,0);
+    c(:)=mean(white_perc_change,2);
+    white_perc_change_surf = gifti;
+    white_perc_change_surf.cdata = c;
+    save(white_perc_change_surf, fullfile(foi_dir, ['white.' comparison_name '.perc_change.shape.gii']), 'ExternalFileBinary');
 
-    % Compare white matter values at two wois
-    [H,pvals,ci,STATS]=ttest(white_diff');
+    % Compute significance of change from baseline in white matter
+    [H,pvals,ci,STATS]=ttest(white_perc_change');
     white_tvals=STATS.tstat';
 
     % Save white matter comparison
@@ -99,7 +103,7 @@ if params.save_results
     save(white_t_surf, fullfile(foi_dir, ['white.' comparison_name '.t.shape.gii']), 'ExternalFileBinary');
 
     % Compare pial and white matter differences
-    pial_white_diff=abs(pial_diff)-abs(white_diff(params.white_pial_map,:));
+    pial_white_diff=abs(pial_perc_change)-abs(white_perc_change(params.white_pial_map,:));
     [H,pvals,ci,STATS]=ttest(pial_white_diff');
     pial_white_tvals=STATS.tstat';
 
