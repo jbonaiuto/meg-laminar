562ff92c3c9c1d3ef071fd0dc51b8c4a39a1cc3b
jbonaiuto <jbonaiuto@gmail.com>
2016-12-12 10:49:01 +0000

    return text

--- layer_comparison/create_subject_layer_evidence_report.m
+++ layer_comparison/create_subject_layer_evidence_report.m
@@ -1,7 +1,7 @@
-function create_subject_layer_evidence_report(subj_info, zero_event, foi, comparison_name, view, varargin)
+function tpl_txt=create_subject_layer_evidence_report(subj_info, zero_event, foi, comparison_name, view, varargin)
 
 % Parse inputs
-defaults = struct('data_dir', '/data/pred_coding', 'url_prefix', 'http://fortressofjollitude.zapto.org/', 'inv_type', 'EBB', 'patch_size',0.4, 'surf_dir', '');  %define default values
+defaults = struct('data_dir', '/data/pred_coding', 'url_prefix', 'http://fortressofjollitude.zapto.org/pred_coding/', 'inv_type', 'EBB', 'patch_size',0.4, 'surf_dir', '');  %define default values
 params = struct(varargin{:});
 for f = fieldnames(defaults)',
     if ~isfield(params, f{1}),
@@ -12,7 +12,7 @@ if length(params.surf_dir)==0
     params.surf_dir=fullfile(params.data_dir,'surf');
 end
 
-addpath C:/users/jbonai/Documents/MATLAB/m2html/
+addpath /home/jbonaiuto/Documents/MATLAB/m2html/
 
 tpl = template('templates/','keep');
 tpl = set(tpl,'file',{'subject'},{'subject_layer_evidence_template.tpl'});
@@ -33,6 +33,7 @@ mkdir(report_dir);
 out_file=fullfile(report_dir,sprintf('layer_evidence_%s.html',comparison_name));
 fid=fopen(out_file,'w');
 %display(get(tpl,'OUT'))
-fprintf(fid, strrep(strrep(get(tpl,'OUT'),'\','/'),'%','%%'));
+tpl_txt=strrep(strrep(get(tpl,'OUT'),'\','/'),'%','%%');
+fprintf(fid, tpl_txt);
 fclose(fid);
 
