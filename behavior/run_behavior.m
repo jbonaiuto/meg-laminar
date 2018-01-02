a747379b6d7c83170ec2c2f63c691748c8962abe
jbonaiuto <jbonaiuto@gmail.com>
2018-01-02 18:38:58 +0100

    updated directory structure to match bids format

--- behavior/run_behavior.m
+++ behavior/run_behavior.m
@@ -8,11 +8,11 @@ for f = fieldnames(defaults)',
     end
 end
 
-load(fullfile('C:\pred_coding\scanning\', subj_info.subj_id, ...
-    num2str(session_num), sprintf('data_%d.mat', run_num)));
+load(fullfile('C:\pred_coding\', subj_info.subj_id, ...
+    sprintf('ses-0%d', session_num), 'behavior', sprintf('data_%d.mat', run_num)));
 
-load(fullfile('C:\pred_coding\scanning\', subj_info.subj_id, ...
-    num2str(session_num), sprintf('stim_%d.mat', run_num)));
+load(fullfile('C:\pred_coding\', subj_info.subj_id, ...
+    sprintf('ses-0%d', session_num), 'behavior', sprintf('stim_%d.mat', run_num)));
 
 % Correct for left/right mismatch
 stim.trials(:,1)=1+2-stim.trials(:,1);
